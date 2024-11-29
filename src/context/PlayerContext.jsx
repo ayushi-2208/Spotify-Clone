52594b6948b1faba386558547afda53dd16c4eda
Ayushi <ayushigupta283203@gmail.com>
2024-11-29 15:36:20 +0530

    Update displayAlbum, SongItem

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useRef, useState } from "react";
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -32,6 +33,31 @@ const PlayerContextProvider = (props) =>{
         setPlayStatus(false)
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id])
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    useEffect(()=>{
+        setTimeout(() =>{
+            audioRef.current.ontimeupdate = () =>{
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100)) + "%";
+               setTime({
+                currentTime:{
+                    second: Math.floor(audioRef.current.currentTime % 60),
+                    minute: Math.floor(audioRef.current.currentTime / 60),
+                },
+                totalTime:{
+                    second: Math.floor(audioRef.current.duration % 60),
+                    minute: Math.floor(audioRef.current.duration / 60),
+                }
+               })
+            }
+        },1000)
+
+    },[audioRef])
+
       const contextValue = {
          audioRef,
          seekBg,
@@ -41,7 +67,8 @@ const PlayerContextProvider = (props) =>{
          playStatus,
          setPlayStatus,
          time,setTime,
-         play,pause
+         play,pause,
+         playWithId
       }
       return (
         <PlayerContext.Provider value={contextValue}>
